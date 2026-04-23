57c557b5c43df4f75d3c0e9669a5f94ecb4d4b8e
aneelaanum-collab <aneela.anum.gondle@gmail.com>
2026-04-23 11:56:09 +0200

    Add game functionality

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@
 
 // Variables
 
-let score = 0
-let timeLeft = 60
+let score = 0;
+let timeLeft = 60;
+let gameStarted = false;
 
 //HTML DOM
 const button1 = document.getElementById('button1');
@@ -14,6 +15,9 @@ const timerDisplay = document.getElementById('timerDisplay');
 // UI Function
 button1.addEventListener('click', () => {
   increaseScore();
+  if(!gameStarted) {
+    startGame();
+  }
 })
 // TODO: stat only when "Clicked Me is Clicked"
 setInterval(countdown, 1000);
@@ -31,3 +35,10 @@ function countdown() {
 }
 // TODO: stop timer at the end
 
+function startGame(){
+  setInterval(countdown, 1000);
+  gameStarted = true;
+}
+function endGame(){
+  
+}
