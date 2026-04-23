c30b183c4ea7b0db6a4141e118c86f5cb74d2d93
aneelaanum-collab <aneela.anum.gondle@gmail.com>
2026-04-23 11:25:19 +0200

    AddCountDown Timer

--- js/app.js
+++ js/app.js
@@ -5,18 +5,29 @@
 // Variables
 
 let score = 0
-const scoreDisplay = document.getElementById('scoreDisplay')
-//let score;
+let timeLeft = 60
 
 //HTML DOM
 const button1 = document.getElementById('button1');
-
+const scoreDisplay = document.getElementById('scoreDisplay');
+const timerDisplay = document.getElementById('timerDisplay');
 // UI Function
 button1.addEventListener('click', () => {
   increaseScore();
 })
+// TODO: stat only when "Clicked Me is Clicked"
+setInterval(countdown, 1000);
+
+
+// Function
 function increaseScore() {
   score++;
   scoreDisplay.innerText = score;
   //console.log(score);
 }
+function countdown() {
+  timeLeft--;
+  timerDisplay.innerText = timeLeft;
+}
+// TODO: stop timer at the end
+
